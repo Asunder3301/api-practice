ce59ca7c4b0fc79508622cb256dc0e7d8509183e
asunder <trashemail3301@gmail.com>
2026-06-30 17:42:35 -0400

    Load new gif when button is clicked
    Src attribute is overwritten before each clicked
    to ensure new gif is loaded properly.

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ fetch(
   .then(function (response) {
     image.src = response.data.images.original.url;
   });
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  image.src = "";
+
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=dVAHLFdSVflST46wwWAhxRHQruM4pawq&s=cats"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      image.src = response.data.images.original.url;
+    });
+});
