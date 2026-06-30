4c370a9141ec6c47cca9570d3d4a920e9a481dff
asunder <trashemail3301@gmail.com>
2026-06-30 17:05:12 -0400

    Display cat gif upon refresh

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.css";
 import "./modern-normalize.css";
+
+const image = document.querySelector("img");
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=dVAHLFdSVflST46wwWAhxRHQruM4pawq&s=cats"
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    image.src = response.data.images.original.url;
+  });
