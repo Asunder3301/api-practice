e1a0c652014d1bc2f30232824cc4a6a8a3dcaa9c
asunder <trashemail3301@gmail.com>
2026-07-01 15:39:22 -0400

    Replace promise syntax with async/await
    Code is more straightforward to read/understand.

--- src/modules/giphyapi.js
+++ src/modules/giphyapi.js
@@ -1,20 +1,19 @@
 export class Giphy {
-  static fetchGif(element, searchTerm) {
+  static async fetchGif(element, searchTerm) {
     if (!searchTerm) return;
-
     element.src = "";
-    fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=dVAHLFdSVflST46wwWAhxRHQruM4pawq&s=${searchTerm}`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("HTTP error! status: ", response.status);
-        }
-        return response.json();
-      })
-      .then((response) => {
-        element.src = response.data.images.original.url;
-      })
-      .catch((error) => console.log("Error fetching GIF: ", error));
+
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=dVAHLFdSVflST46wwWAhxRHQruM4pawq&s=${searchTerm}`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const imageData = await response.json();
+      element.src = imageData.data.images.original.url;
+    } catch (error) {
+      console.log("Error fetching GIF: ", error);
+    }
   }
 }
