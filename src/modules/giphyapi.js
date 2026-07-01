42c19fcaa450580e9958866b3e3e37515cfedef9
asunder <trashemail3301@gmail.com>
2026-07-01 11:53:27 -0400

    Handle undesired responses from API
    "404 Not Found" or similar respones wil be handled
    instead of letting them pass into the second then
    statement.

--- src/modules/giphyapi.js
+++ src/modules/giphyapi.js
@@ -6,10 +6,15 @@ export class Giphy {
     fetch(
       `https://api.giphy.com/v1/gifs/translate?api_key=dVAHLFdSVflST46wwWAhxRHQruM4pawq&s=${searchTerm}`
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP error! status: ", response.status);
+        }
+        return response.json();
+      })
       .then((response) => {
         element.src = response.data.images.original.url;
       })
-      .catch((error) => console.log("Error fetching GIF:", error));
+      .catch((error) => console.log("Error fetching GIF: ", error));
   }
 }
